6444c27693be3cac1b23e3f87f5e2270ed0e0db6
SezerKenar <kenarsezer08@gmail.com>
2021-01-16 21:02:57 +0300

    MekanBul

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,8 +4,21 @@ var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
 
 router.get('/',ctrlMekanlar.anaSayfa);
-router.get('/mekan',ctrlMekanlar.mekanBilgisi);
-router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
+router.get('/mekan/:mekanid',ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/:mekanid/yorum/yeni',ctrlMekanlar.yorumEkle);
 router.get('/hakkinda',ctrlDigerleri.hakkinda);
+router.post('/mekan/:mekanid/yorum/yeni',ctrlMekanlar.yorumumuEkle);
 
+/*
+router.get('/admin/mekan/yeni',ctrlMekanlar.adminSayfa);
+router.get('/admin/mekan/yeni',ctrlMekanlar.mekanEkle);
+//mekanı ekle tuşunun çağıracağı metodun rotası
+router.post('/admin/mekan/yeni',ctrlMekanlar.mekaniEkle);
+//mkean sil
+router.get('/admin/mekan/:mekanid/sil',ctrlMekanlar.mekanSil);
+//mekanı güncelleme sayfa rota
+router.get('/admin/mekan/:mekanid/guncelle',ctrlMekanlar.mekanGuncelle);
+//mekan güncelleme butonununçağıracağı metod rota
+router.post('/admin/mekan/:mekanid/guncelle',ctrlMekanlar.mekaniGuncelle);
+*/
 module.exports = router;
